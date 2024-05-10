ad4b22fd6b6e0c66da6f959bd7e0fe202b52f437
jsswpt <paiusov.d@yandex.ru>
2024-05-10 16:41:36 +0500

    feat(traffic): set initial cars count

--- src/entities/templates/traffic/model.ts
+++ src/entities/templates/traffic/model.ts
@@ -1,9 +1,30 @@
+import { getRandomInt } from "../../../utils/get-random-int";
+
 export type TrafficModelParams = {};
 
 export class TrafficModel {
-  constructor(params?: TrafficModelParams) {}
+  private _carsCount: number | null;
+
+  constructor(params?: TrafficModelParams) {
+    this._carsCount = null;
+  }
+
+  get carsCount(): number | null {
+    return this._carsCount;
+  }
+
+  set carsCount(value: number) {
+    if (value !== this.carsCount) {
+      this._carsCount = value;
+      console.log(value);
+    }
+  }
+
+  private setInitialCarsCount() {
+    this.carsCount = getRandomInt(20, 240, 2);
+  }
 
   init() {
-    console.log("TrafficModel init");
+    this.setInitialCarsCount();
   }
 }
