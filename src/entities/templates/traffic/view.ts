7d951be32f39ae3f84deb44a54c5c227ec2f74c0
jsswpt <paiusov.d@yandex.ru>
2024-05-12 17:42:19 +0500

    feat(traffic): add wrappers

--- src/entities/templates/traffic/view.ts
+++ src/entities/templates/traffic/view.ts
@@ -1,3 +1,5 @@
+import { AvailableRoads } from "../../../shared/data/road";
+
 export type TrafficViewParams = {};
 
 export class TrafficView {
@@ -10,12 +12,29 @@ export class TrafficView {
     return this._id;
   }
 
+  private createRoadWrappers() {
+    const elementsAsString = Object.entries(AvailableRoads).map(
+      (item, idx, arr) => `
+    <div class='side'>
+      <div class='road-wrap' id='road-${item[1]}-wrap'></div>
+    </div>
+    ${idx < arr.length - 1 ? `<div class='separator'></div>` : ``}
+    `,
+    );
+
+    return elementsAsString.join("");
+  }
+
   private createElement() {
     const wrapper = document.getElementById("app");
 
     const trafficWrapper = document.createElement("div");
     trafficWrapper.id = this.id;
     trafficWrapper.className = "traffic";
+
+    const elements = this.createRoadWrappers();
+
+    trafficWrapper.innerHTML = elements;
 
     wrapper?.appendChild(trafficWrapper);
   }
